d39c55704f02273fa4ad34b4291b19d2d5c303b9
shahriar <hsleonis@gmail.com>
2016-08-11 11:58:30 +0600

    Crop image bug fixed

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ require_once ('class.imageresizer.php');
 
 // Create thumbnails
 $args = array(
-    'height'    => 350,
-    'width'     => 250,
+    'height'    => 975,
+    'width'     => 650,
     'is_crop_hard' => 1
 );
 $img = new ImageResizer($args);
